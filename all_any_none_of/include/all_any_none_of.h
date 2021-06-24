e2b24647a9ed159fa6eba319751ffba7b19b5129
João Filipe Silva <joaofilipenasc@gmail.com>
2021-06-23 21:56:26 -0300

    Att

--- all_any_none_of/include/all_any_none_of.h
+++ all_any_none_of/include/all_any_none_of.h
@@ -11,33 +11,42 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ *  @param first Ponteiro apontando para o início do range
+ *  @param last Ponteiro apontando para o fim do range
+ *  @param p Função que retorna true quando uma determinada condição é satisfeita ou falso caso contrário
  */
+
 template<class InputIt, class UnaryPredicate>
-bool all_of(InputIt first, InputIt last, UnaryPredicate p)
-{
-    // TODO
+bool all_of(InputIt first, InputIt last, UnaryPredicate p) {
+    while (first != last) {
+        if (!p(*first)) {
+            return false;
+        }
+        ++first;
+    }
+    return true;
+}
+
+template<class InputIt, class UnaryPredicate>
+bool any_of(InputIt first, InputIt last, UnaryPredicate p) {
+    while (first != last) {
+        if (p(*first)) {
+            return true;
+        }
+        ++first;
+    }
     return false;
 }
 
-/*! 
- * TODO: documentação no estilo doxygen
- */
 template<class InputIt, class UnaryPredicate>
-bool any_of(InputIt first, InputIt last, UnaryPredicate p)
-{
-    // TODO
-    return false;
-}
-
-/*! 
- * TODO: documentação no estilo doxygen
- */
-template<class InputIt, class UnaryPredicate>
-bool none_of(InputIt first, InputIt last, UnaryPredicate p)
-{
-    // TODO
-    return false;
+bool none_of(InputIt first, InputIt last, UnaryPredicate p) {
+    while (first != last) {
+        if (p(*first)) {
+            return false;
+        }
+        ++first;
+    }
+    return true;
 }
 
 }
