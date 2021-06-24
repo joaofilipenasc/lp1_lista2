750fbea177585cd2242285aec1424f1ae4df99cb
João Filipe Silva <joaofilipenasc@gmail.com>
2021-06-24 20:49:40 -0300

    Att

--- rotate/include/rotate.h
+++ rotate/include/rotate.h
@@ -11,13 +11,30 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * 
  */
 template<class ForwardIt>
-ForwardIt rotate(ForwardIt first, ForwardIt n_first, ForwardIt last)
-{
-    // TODO
-   return last;
+ForwardIt rotate(ForwardIt first, ForwardIt n_first, ForwardIt last) {
+    if(first == n_first) {
+        return last;
+    }
+    if(n_first == last) {
+        return first;
+    }
+    ForwardIt read  = n_first;
+    ForwardIt write = first;
+    ForwardIt next_read = first; // ler a posição de quando read for last
+ 
+   while(read != last) {
+      if(write == next_read) {
+          next_read = read; // descobrir onde está o first
+      }
+      std::inter_swap(write++, read++);
+   }
+ 
+   // rotacionar os demais elementos
+   rotate(write, next_read, last);
+   return write;
 }
 
 }
