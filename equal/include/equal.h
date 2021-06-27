1c488aa9d33d459b89bf5e439b6592b8ca18f3bc
João Filipe Silva <joaofilipenasc@gmail.com>
2021-06-27 10:12:11 -0300

    Att

--- equal/include/equal.h
+++ equal/include/equal.h
@@ -11,22 +11,33 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * 
  */
+
 template<class InputIt1, class InputIt2, class Equal>
-bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, Equal eq)
-{
-    // TODO
-    return false;
+bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, Equal eq) {
+    while (first1 != last1) {
+        if (!eq(*first1,*first2)) {
+            return false;
+        }
+        ++first1; 
+        ++first2;
+    }
+    return true;
 }
 
 template<class InputIt1, class InputIt2, class Equal>
 bool equal(InputIt1 first1, InputIt1 last1,
            InputIt2 first2, InputIt2 last2,
-           Equal eq )
-{
-    // TODO
-    return false;
+           Equal eq ) {
+    while (first1 != last1 && first2 != last2) {
+        if (!eq(*first1,*first2)) {
+            return false;
+        }
+        ++first1;
+        ++first2;
+    }
+    return true;
 }
 
 }
